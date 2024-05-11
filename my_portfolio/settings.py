4dee1e1a0bc8a6df1c108e5c5897828d41c84f7c
Jobin Jose <109533232+JOBIN456@users.noreply.github.com>
2024-05-11 21:16:04 +0530

    deploy

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xrswy78@5nw&=*nz9rx7*=qxq!3c0o71irs*!8ayxr9x(30lez'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
