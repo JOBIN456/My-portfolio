e42331745c0fea3fff783d069065d0874389fcfd
Jobin Jose <109533232+JOBIN456@users.noreply.github.com>
2024-06-05 14:56:54 +0530

    cmt

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -7,5 +7,9 @@
 //         abc.style.backgroundColor='transparent';
 //     }
 // });
+var loader = document.getElementById("pre-loader");
+window.addEventListener("load", function() {
+    loader.style.display = "none";
+});
 
 
